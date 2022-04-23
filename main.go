919a43219c51dd44c31e741eb5ddb289b826a538
xyangqiu <chauncyqiu@foxmail.com>
2022-04-23 16:54:38 +0800

    中间件

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"go_gin_demo/file"
 	"go_gin_demo/router"
+	"net/http"
+	"time"
 )
 
 func init() {
@@ -14,6 +16,24 @@ func init() {
 func main() {
 	//test.GetCfg()
 	//router.Run()
+	http.Handle("/time", timeMiddleware(DownFile))
+	http.ListenAndServe(":80", nil)
+}
+
+func DownFile(w http.ResponseWriter, r *http.Request) {
 
 	file.DownLoadFilePercent("D:\\Code\\Other\\test\\Sekiro.mp4", "Sekiro.mp4")
+}
+
+// 中间件
+func timeMiddleware(next http.HandlerFunc) http.Handler {
+
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		t := time.Now()
+		fmt.Println("r.URL.RawQuery:", r.URL.RawQuery)                                    // r.URL.RawQuery: name=q
+		fmt.Println("timeMiddleware star:", t.UnixNano(), "r.RequestURI::", r.RequestURI) //timeMiddleware star: 1650703718705203600 r.RequestURI:: /time?name=q
+		next.ServeHTTP(w, r)
+		fmt.Println("timeMiddleware end:", time.Since(t)) // timeMiddleware end: 7.9025ms
+	})
+
 }
